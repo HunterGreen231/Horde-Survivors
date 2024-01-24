60ad69185ba770ffa8b3101281314e0591ca3ab7
HunterGreen231 <webdev.huntergreen@gmail.com>
2024-01-23 23:34:13 -0700

    Finished first version of the main menu. Connected play button to first level

---
+++ Game Files/Main Menu/MainMenu.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://Game Files/Level1/level_1_world.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
