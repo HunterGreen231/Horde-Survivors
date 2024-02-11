c2d321351de591e8ec5e8f8b390d69a95ed34289
Hunter Green <webdev.huntergreen@gmail.com>
2024-02-11 05:04:15 -0700

    Added basic health bar, player death logic, basic attack loop, enemy death without animation

--- Game Files/Enemies/Skeleton/base_skeleton.gd
+++ Game Files/Enemies/Skeleton/base_skeleton.gd
@@ -2,8 +2,10 @@ extends CharacterBody2D
 
 # Enemy properties
 var speed: int = 60
+var isPursuing: bool = true
 var playerMage: Node2D
 @onready var skeletonAnimatedSprite = get_node("SkeletonAnimatedSprite")
+
 
 func _ready():
 	# Assuming the player node is named "Player"
@@ -13,11 +15,25 @@ func _ready():
 
 func _process(delta):
 	var direction = (playerMage.global_position - global_position).normalized()
-	
 	skeletonAnimatedSprite.flip_h = direction.x < 0
 	
-	# Update the inherent velocity property
-	velocity = direction * speed
+	if(isPursuing):
+		pursue_player(direction)
+	else:
+		velocity = Vector2(0,0)
 
-	# Call move_and_slide without any arguments
+func pursue_player(direction):
+	velocity = direction * speed
 	move_and_slide()
+
+func _on_skeleton_area_body_entered(body):
+	if (body.name == "PlayerMage"):
+		isPursuing = false
+		body.take_damage()
+
+func _on_skeleton_area_body_exited(body):
+	if (body.name == "PlayerMage"):
+		$PursuitDelay.start()
+
+func _on_pursuit_delay_timeout():
+	isPursuing = true
--- Game Files/Players/player_mage.gd
+++ Game Files/Players/player_mage.gd
@@ -1,18 +1,37 @@
 extends CharacterBody2D
 
 # Player properties
-var speed: int = 200
+var speed: int = 100
+var health: int = 100
+
+var nodesInArea = []
 
 # Animation nodes
 @onready var mageAnim = get_node("MageAnimationPlayer")
+@onready var basicAttackAnim = get_node("AttackGroup/BasicAttack/BasicAttackAnimSprite")
 @onready var mageAnimSprite = get_node("PlayerMageAnimSprite")
+@onready var healthBar = get_node("../../CanvasLayer/HealthBarNode/HealthBar")
 
 func _ready():
 	pass
+	
+func take_damage():
+	health -= 10
+	if (health <= 0):
+		player_death()
+	
+func player_death():
+	get_tree().change_scene_to_file("res://Game Files/Main Menu/MainMenu.tscn")
 
 func _process(delta):
+	healthBar.value = health
+	
+	if(basicAttackAnim.is_playing()):
+		if basicAttackAnim.frame >= 2 && basicAttackAnim.frame <= 5:
+			destroy_enemies_in_basic_attack_area()
+	
 	velocity = Vector2()  # Reset velocity
-
+	
 	# Input handling for movement
 	if Input.is_action_pressed('ui_right'):
 		velocity.x += 1
@@ -39,3 +58,21 @@ func _process(delta):
 # Function to flip the sprite
 func set_sprite_flip(is_flipped: bool):
 	mageAnimSprite.flip_h = is_flipped
+	
+func destroy_enemies_in_basic_attack_area():
+	for node in nodesInArea:
+		node.queue_free()
+	nodesInArea.clear()
+
+func _on_basic_attack_timer_timeout():
+	basicAttackAnim.play("Attack")
+
+func _on_basic_attack_area_area_entered(area):
+	var skeleton = area.get_parent()
+	if not nodesInArea.has(skeleton):
+		nodesInArea.append(skeleton)
+
+func _on_basic_attack_area_area_exited(area):
+	var skeleton = area.get_parent()
+	if nodesInArea.has(skeleton):
+		nodesInArea.erase(skeleton)
